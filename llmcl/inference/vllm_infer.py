32d300f16b38487a85eafd1f30ff274bc919eb0d
lixinlong <lxl2067956020@gmail.com>
2024-12-18 16:57:49 +0800

    [fix&feat] add vllm inference

--- llmcl/inference/vllm_infer.py
+++ llmcl/inference/vllm_infer.py
@@ -19,10 +19,6 @@ from vllm.lora.request import LoRARequest
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-GENERATION_CONFIG = {
-    "max_tokens": 512,
-    "temperature": 0,
-}
 
 
 def load_dataset(data_files:str):
@@ -41,7 +37,12 @@ def infer_task(model:LLM, dataset, task_name, save_path, lora_adapter_path) -> L
     tokenizer = AutoTokenizer.from_pretrained(lora_adapter_path)
     prompts = []
     answers = []
-    sample_params = SamplingParams(**GENERATION_CONFIG)
+    sample_params = SamplingParams(
+        max_tokens=512,
+        temperature=0,
+        repetition_penalty=1.15,
+        stop_token_ids=[tokenizer.eos_token_id]
+    )
     sample_params.stop = [tokenizer.eos_token]
     for data in dataset:
         prompts.append("Input\n" + data.get('prompt') + "\nOutput:\n")
@@ -75,16 +76,16 @@ def evaluate_result(task_results, dataset_name) -> float:
     return 1.0
 
 def main():
-    METHOD = "vanilla"
+    METHOD = "ewc"
     ADAPTER_WEIGHT_PATHS = {
-        "C-STANCE": "output/vanilla/vanilla_C-STANCE_round_2_desc_loss_0.6080",
-        "FOMC": "output/vanilla/vanilla_FOMC_round_2_desc_loss_0.0913",
-        "MeetingBank": "output/vanilla/vanilla_MeetingBank_round_2_desc_loss_1.7218",
-        "Py150": "output/vanilla/vanilla_Py150_round_2_desc_loss_0.1856",
-        "ScienceQA": "output/vanilla/vanilla_ScienceQA_round_2_desc_loss_0.3663",
-        "NumGLUE-cm": "output/vanilla/vanilla_NumGLUE-cm_round_2_desc_loss_0.0888",
-        "NumGLUE-ds": "output/vanilla/vanilla_NumGLUE-ds_round_2_desc_loss_0.0284",
-        "20Minuten": "output/vanilla/vanilla_20Minuten_round_2_desc_loss_1.8274"
+        "C-STANCE": "output/ewc/ewc_C-STANCE_round_2_desc_loss_0.6089",
+        "FOMC": "output/ewc/ewc_FOMC_round_2_desc_loss_0.0927",
+        "MeetingBank": "output/ewc/ewc_MeetingBank_round_2_desc_loss_1.7228",
+        "Py150": "output/ewc/ewc_Py150_round_2_desc_loss_0.1869",
+        "ScienceQA": "output/ewc/ewc_ScienceQA_round_2_desc_loss_0.3688",
+        "NumGLUE-cm": "output/ewc/ewc_NumGLUE-cm_round_2_desc_loss_0.0905",
+        "NumGLUE-ds": "output/ewc/ewc_NumGLUE-ds_round_2_desc_loss_0.0306",
+        "20Minuten": "output/ewc/ewc_20Minuten_round_2_desc_loss_1.8288"
     }
     DATA_PATH = "data/TRACE-Benchmark/LLM-CL-Benchmark_5000"
     OUTPUT_PATH = f'infer_output/{METHOD}'
@@ -95,17 +96,16 @@ def main():
     result_metrics = {}
     for task_name, adapter_path in ADAPTER_WEIGHT_PATHS.items():
         result_metrics[task_name] = {}
-        logger.info(f"using {task_name} adapter weight!!")
         for dataset_name in ADAPTER_WEIGHT_PATHS.keys():
             dataset = load_dataset(os.path.join(DATA_PATH, dataset_name, 'test.json'))
             if os.path.exists(os.path.join("{OUTPUT_PATH}/train_{task_name}", f"{dataset_name}.jsonl")):
                 logger.info(f"skip train: {task_name}, infer: {dataset_name}")
                 continue
-                
+            logger.info(f"method: {METHOD}, train: {task_name}, infer: {dataset_name} inference .......") 
             task_results = infer_task(model, dataset, dataset_name, save_path=f"{OUTPUT_PATH}/train_{task_name}", lora_adapter_path=adapter_path)
             metrics:float= evaluate_result(task_results, dataset_name)
             result_metrics[task_name][dataset_name] = metrics
-            logger.info(f"train: {task_name}, infer: {dataset_name} inference finished")
+            logger.info(f"method: {METHOD}, train: {task_name}, infer: {dataset_name} inference finished")
 
     with open(os.path.join(OUTPUT_PATH, 'result.json'), 'w') as f:
         f.write(json.dumps(result_metrics, ensure_ascii=False, indent=4))
