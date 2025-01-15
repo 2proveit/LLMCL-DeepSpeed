c2355d6ff6044e564dabb228e2c48db90e8562bb
lixinlong <lxl2067956020@gmail.com>
2025-01-15 21:32:23 +0800

    add multi task learning

--- llmcl/methods/__init__.py
+++ llmcl/methods/__init__.py
@@ -1,9 +1,11 @@
 from .vanilla import *
 from .ewc import *
 from .gem import *
+from .mtl import *
 
 TRAINERS = {
     "vanilla": VanillaTrainer,
     "ewc": EWCTrainer,
     "gem": GEMTrainer,
+    'mtl': MultiTaskTrainer
 }
