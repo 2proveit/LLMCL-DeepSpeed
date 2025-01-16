df50bc845c400acc5cfff097e14c6d47ef7fd4aa
lixinlong <lxl2067956020@gmail.com>
2025-01-16 10:19:19 +0800

    add ave gradient eposide memory

--- llmcl/methods/__init__.py
+++ llmcl/methods/__init__.py
@@ -2,10 +2,12 @@ from .vanilla import *
 from .ewc import *
 from .gem import *
 from .mtl import *
+from .agem import *
 
 TRAINERS = {
     "vanilla": VanillaTrainer,
     "ewc": EWCTrainer,
     "gem": GEMTrainer,
-    'mtl': MultiTaskTrainer
+    'mtl': MultiTaskTrainer,
+    'agem': AGEMTrainer
 }
