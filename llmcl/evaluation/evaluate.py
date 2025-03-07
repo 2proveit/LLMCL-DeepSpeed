19e587a134a195dc04833ea7c5a5ef2e457fba08
lixinlong <lxl2067956020@gmail.com>
2025-03-07 10:45:15 +0800

    fix: evaluate

--- llmcl/evaluation/evaluate.py
+++ llmcl/evaluation/evaluate.py
@@ -2,8 +2,9 @@ from typing import Dict
 from metrics import (
     eval_20Minuten, eval_CStance, eval_FOMC, eval_MeetingBank, eval_numGLUE_cm, eval_numGLUE_ds, eval_Py150, eval_ScienceQA
 )
-import json
+import json, re
 import pandas as pd
+import numpy as np
 from pathlib import Path
 eval_fn = {
     "C-STANCE": eval_CStance,
@@ -19,19 +20,43 @@ eval_fn = {
 ZERO_SHOT_PERFORMANCE = {
     "C-STANCE": 0.5475,
     "FOMC": 0.5826612903225806,
-    "MeetingBank": 0.10632118161853012,
-    "Py150": 0.030245,
+    # "MeetingBank": 0.10632118161853012,
+    # "Py150": 0.030245,
     "ScienceQA": 0.352,
     "NumGLUE-cm": 0.8518518518518519,
     "NumGLUE-ds": 0.6,
     "20Minuten": 0.10889139813684952,
 }
 
+def get_train_infer_dataset_name(path:Path, names:list):
+    path = str(path)
+    names.append('MTL')
+    pattern = r'(' + '|'.join(re.escape(name) for name in names) + r')_infer_(' + '|'.join(re.escape(name) for name in names) + r')'
+    matches = re.search(pattern, path, re.DOTALL)
+    if matches:
+        return matches.group(1), matches.group(2)
+    else:
+        raise ValueError((f"Can not match any of {names} for {path}"))
+
+def get_method(path:Path, methods:list):
+    path = str(path)
+    pattern = r'\b(' + '|'.join(re.escape(mtd) for mtd in methods) + r')\b'
+    match = re.search(pattern, path)
+    if match:
+        return match.group(0).strip()
+    else:
+        raise ValueError(f"Can not match any of {methods} for {path}")
+    
 def calculate_metrics(results: Dict[str, Dict[str, float]], incremental_order: list):
     overall_accs = []
     triangle_performance = []
     bwts = []
     fws = []
+    cl_method = list(results.keys())[0]
+    if cl_method == 'MTL':
+        return {
+            'acc': np.mean(list(results[cl_method].values()))
+        }
     assert len(results) == len(incremental_order) and all(len(res) == len(incremental_order) for res in results.values())
     len_tasks = len(incremental_order)
     for i in range(len_tasks):
@@ -59,21 +84,18 @@ def main(json_path):
     for file in json_path.rglob('*'):
         if not file.is_file() or not str(file.name).endswith('jsonl'):
             continue
-        if 'train_' in str(file.parent):
-            trained = str(file.parent).split('train_')[1]
-        else:
-            raise ValueError(f"'train_' not in {file.parent}") 
+        trained, infered  = get_train_infer_dataset_name(file, names=list(eval_fn.keys()))
         if trained not in result:
             result[trained] = {}
         
-        eval_func = eval_fn[file.name[:-6]]
+        eval_func = eval_fn[infered]
         task_results = file.open('r', encoding='utf-8').readlines()
         task_results = [json.loads(d) for d in task_results]
         response = [d['response'] for d in task_results]
         answer = [d['answer'] for d in task_results]
         
         performance = eval_func(response=response, answers=answer)
-        result[trained][file.name[:-6]] = performance
+        result[trained][infered] = performance
     over_all_performance = calculate_metrics(result, incremental_order=list(ZERO_SHOT_PERFORMANCE.keys()))
     result['over_all_performance'] = over_all_performance
 
